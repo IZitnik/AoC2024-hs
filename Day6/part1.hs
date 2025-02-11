ad31da88c458bb42a1f108607eb8b02b82bc963e
Ivo Zitnik <tekkitakcz@gmail.com>
2025-02-11 22:44:35 +0100

    Changed type Guard to struct of Pos Dir

--- Day6/part1.hs
+++ Day6/part1.hs
@@ -2,9 +2,9 @@ module Main where
 import Data.Set (Set, empty, insert, size, member)
 type Pos       = (Int, Int)
 type Bounds    = (Int, Int)
-type Guard     = Pos
 type Obst      = Pos
 data Direction = North | East | South | West deriving (Show, Eq, Enum)
+data Guard     = Guard Pos Direction deriving (Show, Eq)
 data Parsed    = Parsed Bounds Guard (Set Obst) deriving (Show)
 
 getBounds :: String -> Bounds
@@ -13,48 +13,54 @@ getBounds str = let ln@(h:_) = lines str in ((length h)-1, (length ln)-1)
 parse :: String -> Parsed
 parse inp =
   let bounds  = getBounds inp
-      initial = Parsed bounds (0,0) empty
+      guard   = Guard (0,0) North
+      initial = Parsed bounds guard empty
    in foldl parseLine initial $ zip (lines inp) [0..]
   where parseLine :: Parsed -> (String, Int) -> Parsed
         parseLine p (l,n) = foldl (parseChar n) p $ zip l [0..]
         parseChar :: Int -> Parsed -> (Char, Int) -> Parsed
         parseChar row (Parsed b g o) (ch,col)
            | ch == '.'  = Parsed b g o
-           | ch == '#'  = Parsed b g $ insert (row,col) o
-           | ch == '^'  = Parsed b (row,col) o
+           | ch == '#'  = Parsed b g $ insert pos o
+           | ch == '^'  = Parsed b (Guard pos North) o
            | otherwise = error "Unexpected token encountered in parse"
+            where pos = (row, col)
 
-next :: Pos -> Direction -> Pos
-next (y,x) dir = case dir of
-  North -> (y-1, x  )
-  East  -> (y  , x+1)
-  South -> (y+1, x  )
-  West  -> (y  , x-1)
+move :: Guard -> Guard
+move (Guard (y,x) dir) = case dir of
+                           North -> Guard (y-1, x  ) dir
+                           East  -> Guard (y  , x+1) dir
+                           South -> Guard (y+1, x  ) dir
+                           West  -> Guard (y  , x-1) dir
 
-right :: Direction -> Direction
-right West = North
-right dir  = succ dir
+rotate :: Guard -> Guard
+rotate (Guard p dir) = Guard p (right dir)
+  where right :: Direction -> Direction
+        right West = North
+        right dir  = succ dir
+
+position :: Guard -> Pos
+position (Guard p _) = p
 
 inBounds :: Bounds -> Pos -> Bool
 inBounds (bx,by) (x,y) = x>=0 && x<=bx && y>=0 && y<=by
 
-solve' :: Bounds -> Guard -> Set Obst -> Direction -> Set Pos
-solve' b g obs dir = foldl (\b (g,_) -> insert g b) empty $ iterateWhile inB step' (g, dir)
-  where inB :: (Pos, Direction) -> Bool
-        inB = (inBounds b) . fst
-        step' :: (Guard, Direction) -> (Guard, Direction)
-        step' (tG,tDir) = step tG obs tDir
+solve :: Bounds -> Guard -> Set Obst -> Set Pos
+solve b g obs = foldl (\b a -> insert (position a) b) empty $ iterateWhile inB step' g
+  where inB :: Guard -> Bool
+        inB = inBounds b . position
+        step' :: Guard -> Guard
+        step' = step obs
 
 iterateWhile :: (a -> Bool) -> (a -> a) -> a -> [a]
 iterateWhile end fn init = takeWhile end $ iterate fn init
 
-step :: Guard -> Set Obst -> Direction -> (Guard, Direction)
-step g obs dir
-  | obstacleAhead = (g , right dir)
-  | otherwise     = (g', dir      )
+step :: Set Obst -> Guard -> Guard
+step obs guard
+  | obstacleAhead = rotate guard
+  | otherwise     = move   guard
    where obstacleAhead :: Bool
-         obstacleAhead = member g' obs
-         g' = next g dir
+         obstacleAhead = member (position . move $ guard) obs
 
 getBoard :: Bounds -> Set Obst -> Set Pos -> String
 getBoard (bx,by) o t = unlines [[gc (y,x) | x<-[0..bx]] | y<-[0..by]]
@@ -67,7 +73,7 @@ getBoard (bx,by) o t = unlines [[gc (y,x) | x<-[0..bx]] | y<-[0..by]]
 main :: IO ()
 main = do f_content <- readFile "input.txt"
           let (Parsed bounds guard obsts) = parse f_content
-          let traversed = solve' bounds guard obsts North
+          let traversed = solve bounds guard obsts
           -- putStrLn $ getBoard bounds obsts traversed
           print $ size traversed
 
